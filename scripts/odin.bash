bb159217d15c5eae16af4ad7887056d2403c4da7
Tibor Bukovics <tibor.bukovics@ess.eu>
2024-04-24 08:32:42 +0000

    changes to test on odin

--- scripts/odin.bash
+++ scripts/odin.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 BROKER=${DAQLITE_BROKER:-}
+KAFKA_CONFIG=${DAQLITE_KAFKA_CONFIG:-}
 
-../build/bin/daqlite $BROKER -f ../configs/odin/odin.json &
-../build/bin/daqlite $BROKER -f ../configs/odin/odintof.json
+
+
+../build/bin/daqlite $BROKER -f ../configs/odin/odin.json -k $KAFKA_CONFIG &
+../build/bin/daqlite $BROKER -f ../configs/odin/odintof.json -k $KAFKA_CONFIG
