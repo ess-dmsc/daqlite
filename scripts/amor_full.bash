93a8cc8fd53a0a87f2c55923a7a5191ffb42db38
Tibor Bukovics <tibor.bukovics@ess.eu>
2026-03-16 12:35:01 +0000

    scripts: update amor starter scripts

---
+++ scripts/amor_full.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BROKER=${DAQLITE_BROKER:-}
+
+KAFKA_CONFIG=""
+
+# Check if we are in production environment and set KAFKA_CONFIG accordingly
+# or set dev environment variables
+if [ "$DAQLITE_PRODUCTION" = "true" ]; then
+    KAFKA_CONFIG="-k $DAQLITE_CONFIG/kafka-config-daqlite.json"
+else
+    DAQLITE_HOME="../build"
+    DAQLITE_CONFIG="../configs"
+fi
+
+$DAQLITE_HOME/bin/daqlite $BROKER $KAFKA_CONFIG -f $DAQLITE_CONFIG/amor/amor.json &
+$DAQLITE_HOME/bin/daqlite $BROKER $KAFKA_CONFIG -f $DAQLITE_CONFIG/amor/amortof.json &
+$DAQLITE_HOME/bin/daqlite $BROKER $KAFKA_CONFIG -f $DAQLITE_CONFIG/amor/amortof2d.json &
+
+# Beam monitor
+$DAQLITE_HOME/bin/daqlite $BROKER -f $DAQLITE_CONFIG/amor/amor_ibm.json $KAFKA_CONFIG
--- scripts/amorb.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BROKER=${DAQLITE_BROKER:-}
-
-../build/bin/daqlite $BROKER -f ../configs/amorb/amor.json &
-../build/bin/daqlite $BROKER -f ../configs/amorb/amormon.json &
-../build/bin/daqlite $BROKER -f ../configs/amorb/amortof.json &
-../build/bin/daqlite $BROKER -f ../configs/amorb/amortof2d.json
